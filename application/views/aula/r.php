64d0f639bc07a1937afb2f77e29743b216e2eeb5
MonFlowers <49616301+alsoacat@users.noreply.github.com>
2020-06-09 18:23:54 +0200

    r u

--- application/views/aula/r.php
+++ application/views/aula/r.php
@@ -4,46 +4,51 @@
 </div>
 
 
-<table class="highlight striped">
-	<tr>
-		<th>Tipo</th>
-		<th>Nombre</th>
-		<th>Capacidad</th>
-		<th>Comentarios</th>
-		<th>Acción</th>
-	</tr>
-	
+	<div class="row">
 	<?php foreach ($aulas as $aula): ?>
-		<tr>
-		<td><?= $aula->pertenece->nombre?></td>
-		<td><?= $aula->nombre?></td>
-		<td><?= $aula->capacidad?></td>
-		<td><?= $aula->comentarios!=null?$aula->comentarios:'--'?></td>
 	
-		<td>
-		<div class="row">
-			<div class="col m2">
-			<form action="<?=base_url()?>aula/d" method="post">
-				<input type="hidden" name="id" value="<?=$aula->id?>">
-				<button onclick="submit()" class="btn-floating waves-effect waves-light">
-					<i class="large material-icons">delete</i>
-				</button>
-			</form>
-			</div>
-			<div class="col m2">
-			<form action="<?=base_url()?>aula/u" method="post">
-				<input type="hidden" name="id" value="<?=$aula->id?>">
-				<button onclick="submit()" class="btn-floating waves-effect waves-light">
-					<i class="large material-icons">edit</i>
-				</button>
-			</form>
-			</div>
-			</div>
-		</td>
+	<div class="col s12 m4 l4 xl4"> 
+		 <div class="card medium">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="https://cdn.pixabay.com/photo/2020/05/22/08/23/burma-5204371_1280.png">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4"><?= $aula->nombre?><i class="material-icons right">more_vert</i></span>
+      <p>Capacidad: <?=$aula->capacidad?></p><br>
+      <p>Observaciones: <?= $aula->comentarios ? $aula->comentarios  : '--'?></p>
+      <a href="<?=base_url()?>aula/c" class="btn waves-effect waves-light light-green lighten-1">Reservar</a>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><?= $aula->nombre?><i class="material-icons right">close</i></span>
+      <a href="#" class="btn waves-effect waves-light light-green lighten-1">Ver Reservas de este Aula</a>
+    </div>
+  </div>
+		</div>
+		
+			<?php endforeach;?>
+		
+		</div>
+<!-- 		<div class="row"> -->
+<!-- 			<div class="col m2"> 
+			<form action="<//base_url()>aula/d" method="post">
+				<input type="hidden" name="id" value="<//$aula->id?>">
+				<button onclick="submit()" class="btn-floating waves-effect waves-light">-->
+<!-- 					<i class="large material-icons">delete</i> -->
+<!-- 				</button> -->
+<!-- 			</form> -->
+<!-- 			</div> -->
+<!-- 			<div class="col m2"> 
+			<form action="<//base_url()?>aula/u" method="post">
+				<input type="hidden" name="id" value="</->id?>">
+				<button onclick="submit()" class="btn-floating waves-effect waves-light">-->
+<!-- 					<i class="large material-icons">edit</i> -->
+<!-- 				</button> -->
+<!-- 			</form> -->
+<!-- 			</div> -->
+<!-- 			</div> -->
+		
 
-	</tr>
-	<?php endforeach;?>
-</table>
+
 
 
  	
